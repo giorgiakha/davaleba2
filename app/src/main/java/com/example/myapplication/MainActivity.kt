2e9c0d85425c0414d3da86ba90bd2fddf88a84cb
giorgi <giorgi.akhalai.1@btu.edu.com>
2021-11-26 16:46:20 +0400

    rame

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editText3: EditText
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText3 = findViewById(R.id.editText3)
+        button = findViewById(R.id.button1)
+
+        button.setOnClickListener {
+            val ragaca3 =editText3.text.toString()
+            val intent = Intent(this,MainActivity2:: class.java)
+            intent.putExtra("მომხმარებლის სახელი",ragaca3)
+
+
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity2 : AppCompatActivity() {
+    val rightmeil = "akhalaiagiorgi.17@gmail.com"
+    val rightpassword = "giorgi"
+    private lateinit var meil:EditText
+    private lateinit var password:EditText
+    private lateinit var login: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        findViewById<TextView>(R.id.editText6).text = intent?.extras?.getString("მომხმარებლის სახელი","")
+        meil = findViewById(R.id.gio4)
+        password = findViewById(R.id.gio2)
+        login = findViewById(R.id.gio3)
+
+    }
+    fun login(login:View){
+        if (meil.text.toString()==rightmeil.toString()){
+            if (password.text.toString()==rightpassword.toString()){
+                Toast.makeText(this, "corect", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "incorect", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else{
+            Toast.makeText(this, "თქვენი მეილი ან პაროლი არასწორია", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
